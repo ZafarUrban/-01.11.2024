d20208894ffe86e3c50bf2ae93c1f622ab847f05
ZafarUrban <tumanzoda@gmail.com>
2024-11-01 16:50:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+print("Арифметика")
+print('1st ' + 'program') # 1st program
+print(int(9**0.5*5))
+print('=========================')
+print("Логика")
+print('2nd ' + 'program') # 2nd program
+print(9.99 >= 9.98 and 1000 != 1000.1)
+print('=========================')
+print("Школьная загадка")
+print('3rd ' + 'program') # 3rd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2) == (2*(2+2)))
+print('=========================')
+print("Первый после точки")
+print('4th ' + 'program') # 4th program
+print(type(123.456))
+print(str(123.456))
+print(123.456*10)
+print(123.456*10 % 15)
+print(int(123.456*10 % 15))
+print('=========================')
