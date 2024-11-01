66fc849a913c6874e2b5858320b1565dadb2bb03
ZafarUrban <tumanzoda@gmail.com>
2024-11-01 17:01:53 +0300

    Add files via upload

---
+++ datas.py
@@ -0,0 +1,41 @@
+from itertools import product
+from runpy import run_path
+
+print(5)
+print(type(5)) # int - integer
+print(5+5)
+print(5-5)
+print(5*5)
+print(5 / 5) # /  - деление
+print(5 // 5) # // - целосичленное деление
+print(5 % 5) # - % - мы можем найти остаток от деления
+print(5 ** 2) # ** - степень
+print(2.0) # float - число с плавящей точкой или с плавящей запятой
+print(type(2.0))
+print(2.0 + 2)
+print('Hello, World!')
+print(type('Hello, World!')) # - string - строка, используем кавычки для обозначение строк
+print("Hello, World!")
+print('Hello, "World!"') # - обрати внимание на двойную кавычку
+print("Hello, 'World!'") # - обрати внимание на одинарную кавычку
+print('Hello,'+ 'World!') # -  concatenate
+print('1' + '1')
+print(type(True), type(False)) # boolean - логический тип данных
+print(5 > 10, 5 < 10) # boolean
+print(1,2,3,4,'Hello World',True)
+print(5 <= 5)
+print(5 >= 5)
+print(5 == 5) # для того чтобы проверить равны ли эти объекты (равны?)
+print(5 != 5) # - для того чтобы увидеть объекты равны или нет '!' (не равны?)
+print(5 != 5 and 5 < 10)
+print(5 == 5 and 5 < 10)
+print(5 == 5 or 5 < 10)
+print(5 != 5 or 5 < 10)
+print(5 != 5 or 5 > 10)
+print(type(int('5')))
+print(type(str('5')))
+print(type(float('5')))
+print(type(bool('5')))
+
+
+
