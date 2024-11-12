c93ee7c8f3e0492a95b3be6d495236ee24e7604c
ZafarUrban <tumanzoda@gmail.com>
2024-11-12 09:49:03 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,14 @@
+my_string=input('Название учебного заведения, где вы учитесь: ')
+print('Количество символов:',len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
+
+my_string='Название учебного заведения, где вы учитесь'
+print('Название учебного заведения, где вы учитесь'.upper())
+print('Название учебного заведения, где вы учитесь'.lower())
+print('Название учебного заведения, где вы учитесь'.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
